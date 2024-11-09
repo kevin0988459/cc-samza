5795f4da2fff4db2cbd1ea826cf6435bb5e47635
kevin0988459 <kevin0988459@gmail.com>
2024-11-09 04:52:37 -0500

    t

--- src/test/java/com/cloudcomputing/samza/nycabs/TestDriverMatchTask.java
+++ src/test/java/com/cloudcomputing/samza/nycabs/TestDriverMatchTask.java
@@ -65,36 +65,9 @@ public class TestDriverMatchTask {
         System.out.println(rightBlockTest.get("driverId").toString());
         Assert.assertTrue(rightBlockTest.get("clientId").toString().equals("7")
                 && rightBlockTest.get("driverId").toString().equals("3002"));
-    }
-
-    // I didnt see the TODO of the required test so I made this one based on my understanding of the requirements
-    @Test
-    public void testBlockIdAndAvailabilityMatching() throws Exception {
-        Map<String, String> confMap = new HashMap<>();
-        confMap.put("stores.driver-loc.factory", "org.apache.samza.storage.kv.RocksDbKeyValueStorageEngineFactory");
-        confMap.put("stores.driver-loc.key.serde", "string");
-        confMap.put("stores.driver-loc.msg.serde", "json");
-        confMap.put("serializers.registry.json.class", "org.apache.samza.serializers.JsonSerdeFactory");
-        confMap.put("serializers.registry.string.class", "org.apache.samza.serializers.StringSerdeFactory");
-
-        InMemorySystemDescriptor isd = new InMemorySystemDescriptor("kafka");
-
-        InMemoryInputDescriptor imdriverLocation = isd.getInputDescriptor("driver-locations", new NoOpSerde<>());
-        InMemoryInputDescriptor imevents = isd.getInputDescriptor("events", new NoOpSerde<>());
-        InMemoryOutputDescriptor outputMatchStream = isd.getOutputDescriptor("match-stream", new NoOpSerde<>());
-
-        TestRunner
-                .of(new DriverMatchTaskApplication())
-                .addInputStream(imevents, TestUtils.genStreamData("events"))
-                .addInputStream(imdriverLocation, TestUtils.genStreamData("driver-locations"))
-                .addOutputStream(outputMatchStream, 1)
-                .addConfig(confMap)
-                .addConfig("deploy.test", "true")
-                .run(Duration.ofSeconds(5));
-
-        ListIterator<Object> resultIter = TestRunner.consumeStream(outputMatchStream, Duration.ofSeconds(10)).get(0).listIterator();
         Map<String, Object> matchTest = (Map<String, Object>) resultIter.next();
-        System.out.println("Actual match result: " + matchTest);
+        // I didnt s the TODO of the required test so I made this one based on my understanding of the requirements
+        // new mock data is added to events and driverlocations
         Assert.assertTrue(matchTest.get("clientId").toString().equals("10")
                 && matchTest.get("driverId").toString().equals("1001"));
     }
